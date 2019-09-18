4b55518f627d80c3996ebc6449d16e9f1ed4e519
thehardwareguy2000 <54811767+thehardwareguy2000@users.noreply.github.com>
2019-09-18 22:53:52 +0530

    text.py
    this is img to text conversion

--- redworddetect.py
+++ redworddetect.py
@@ -1,64 +1,38 @@
 import cv2
-
 import numpy as np
-
-import matplotlib.pyplot as plt
-
-import os
-%matplotlib inline
-
-os.chdir('C:/Users/Gavin/Desktop')
-
-img = cv2.imread('image2.jpg')
-
-
-plt.imshow(img)
-
-
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-plt.imshow(hsv)
-
-
-# Range for lower red
-lower_red = np.array([0,120,70])
-upper_red = np.array([10,255,255])
-mask1 = cv2.inRange(hsv, lower_red,upper_red)
-
-# Range for upper range
-lower_red = np.array([170,120,70])
-upper_red = np.array([180,255,255])
-mask2 = cv2.inRange(hsv,lower_red,upper_red)
-
-# Generating the final mask to detect red color
-mask1 = mask1+mask2
-
-#identify text with black background
-dst = cv2.bitwise_and(img, img, mask=mask1)
-
-#identify text with grey scale image
-res = cv2.bitwise_not(mask1)
-
-plt.imshow(dst)
-
-plt.imshow(res)
-
-cv2.imwrite("dst.jpg", dst)
-
-cv2.imwrite("res.jpg", res)
-
-###############################################################################
-
-
-from PIL import Image 
 import pytesseract
+from PIL import Image
+from pytesseract import image_to_string
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-text1 = str(((pytesseract.image_to_string(Image.open("res.jpg"),lang = 'eng'))))
 
-text2 = str(((pytesseract.image_to_string(Image.open("dst.jpg"),lang = 'eng'))))
-print("text is:-")
-print(text1)
-print("this is secondary text:- ")
-print(text2)
+def get_string(img_path):
+   
+    img = cv2.imread("img.jpg")
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # noise reduction
+    cv2.imwrite(src_path + "removed_noise.png", img)
+
+    # threshold:- img to b&w 
+    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+    cv2.imwrite(src_path + "thres.png", img)
+    
+    result = pytesseract.image_to_string(Image.open(src_path + "thres.png"))
+
+   
+    return result
+
+
+print('below is then text thats read')
+print('-----------------------------------------------------------')
+print(get_string(src_path + "cont.jpg") )
+print('-----------------------------------------------------------')
+print("The end")
