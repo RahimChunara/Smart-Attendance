85c2f8ae862b099b6b9ca747ac6d95ee1dc5fd10
thehardwareguy2000 <54811767+thehardwareguy2000@users.noreply.github.com>
2019-09-25 22:08:36 +0530

    Update redworddetect.py

--- redworddetect.py
+++ redworddetect.py
@@ -1,64 +1,97 @@
+from imutils.perspective import four_point_transform
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
 import cv2
 
-import numpy as np
-
-import matplotlib.pyplot as plt
-
-import os
-%matplotlib inline
-
-os.chdir('C:/Users/Gavin/Desktop')
-
-img = cv2.imread('image2.jpg')
 
 
-plt.imshow(img)
+image = cv2.imread("test_01.png")
+cv2.imshow("image",image)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(blurred, 75, 200)
 
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+docCnt = None
 
-plt.imshow(hsv)
+if len(cnts) > 0:
 
-
-# Range for lower red
-lower_red = np.array([0,120,70])
-upper_red = np.array([10,255,255])
-mask1 = cv2.inRange(hsv, lower_red,upper_red)
-
-# Range for upper range
-lower_red = np.array([170,120,70])
-upper_red = np.array([180,255,255])
-mask2 = cv2.inRange(hsv,lower_red,upper_red)
-
-# Generating the final mask to detect red color
-mask1 = mask1+mask2
-
-#identify text with black background
-dst = cv2.bitwise_and(img, img, mask=mask1)
-
-#identify text with grey scale image
-res = cv2.bitwise_not(mask1)
-
-plt.imshow(dst)
-
-plt.imshow(res)
-
-cv2.imwrite("dst.jpg", dst)
-
-cv2.imwrite("res.jpg", res)
-
-###############################################################################
+	cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
 
-from PIL import Image 
-import pytesseract
+	for c in cnts:
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
-text1 = str(((pytesseract.image_to_string(Image.open("res.jpg"),lang = 'eng'))))
 
-text2 = str(((pytesseract.image_to_string(Image.open("dst.jpg"),lang = 'eng'))))
-print("text is:-")
-print(text1)
-print("this is secondary text:- ")
-print(text2)
+		if len(approx) == 4:
+			docCnt = approx
+			break
+else :
+    print("get the picture of full image")
+paper = four_point_transform(image, docCnt.reshape(4, 2))
+warped = four_point_transform(gray, docCnt.reshape(4, 2))
+
+# apply Otsu's thresholding method to binarize the warped
+# piece of paper
+thresh = cv2.threshold(warped, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+questionCnts = []
+
+
+for c in cnts:
+	
+	(x, y, w, h) = cv2.boundingRect(c)
+	ar = w / float(h)
+
+	
+	if w >= 20 and h >= 20 and ar >= 0.9 and ar <= 1.1:
+		questionCnts.append(c)
+
+
+questionCnts = contours.sort_contours(questionCnts,method="top-to-bottom")[0]
+correct = 0
+
+
+for (q, i) in enumerate(np.arange(0, len(questionCnts), 5)):
+	
+	cnts = contours.sort_contours(questionCnts[i:i + 5])[0]
+	bubbled = None
+
+	for (j, c) in enumerate(cnts):
+		
+		mask = np.zeros(thresh.shape, dtype="uint8")
+		cv2.drawContours(mask, [c], -1, 255, -1)
+    
+		mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+		total = cv2.countNonZero(mask)
+
+		if bubbled is None or total > bubbled[0]:
+			bubbled = (total, j)
+
+	color = (0, 0, 255)
+	k = ANSWER_KEY[q]
+
+	if k == bubbled[1]:
+		color = (0, 255, 0)
+		correct += 1
+    score = (correct / 5.0) * 100
+	
+	cv2.drawContours(paper, [cnts[k]], -1, color, 3)
+
+
+#score = (correct / 5.0) * 100
+print("[INFO] score: {:.2f}%".format(score))
+cv2.putText(paper, "{:.2f}%".format(score), (10, 30),
+	cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+cv2.imshow("Original", image)
+cv2.imshow("Exam", paper)
+cv2.waitKey(0)
+
